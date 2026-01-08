d008d3389122381aa65943d9818470934de1b2d4
unknown <oanhtt@hblab/vn>
2026-01-08 15:34:24 +0700

    add docs

---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Project'
+copyright = '2026, Author'
+author = 'Author'
+
+version = '0.1'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Allow Markdown files using MyST-Parser
+source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
+
+# MyST-Parser configuration
+myst_enable_extensions = [
+    "deflist",
+    "html_admonition",
+    "html_image",
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
